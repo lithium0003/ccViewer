8edb896fdca83ea1b32547a2e1d389e153648c46
lithium03 <contact@lithium03.info>
2019-11-15 22:20:38 +0900

    add Chromecast SDK

---
+++ chromecast.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -OL https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-no-bluetooth-4.4.6_dynamic.zip
+unzip GoogleCastSDK-ios-no-bluetooth-4.4.6_dynamic.zip
+ln -s GoogleCastSDK-ios-4.4.6_dynamic GoogleCastSDK
+
