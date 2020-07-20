ffb5e2652ae54c2eaf1d145a38c0345c2a60bc69
lithium03 <contact@lithium03.info>
2020-07-21 01:05:29 +0900

    update ChromeCastSDK

--- chromecast.sh
+++ chromecast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -OL https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-no-bluetooth-4.4.6_dynamic.zip
-unzip GoogleCastSDK-ios-no-bluetooth-4.4.6_dynamic.zip
-ln -s GoogleCastSDK-ios-4.4.6_dynamic GoogleCastSDK
+curl -OL https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-no-bluetooth-4.4.7_dynamic.zip
+unzip GoogleCastSDK-ios-no-bluetooth-4.4.7_dynamic.zip
+ln -s GoogleCastSDK-ios-4.4.7_dynamic GoogleCastSDK
 patch -p0 <strip.diff
