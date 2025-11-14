9ace0e99a283cf9a14176cd1a5034a7409997a92
lithium03 <contact@lithium03.info>
2025-11-14 16:25:39 +0900

    fix

--- chromecast.sh
+++ chromecast.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl -OL https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-no-bluetooth-4.4.7_dynamic.zip
-unzip GoogleCastSDK-ios-no-bluetooth-4.4.7_dynamic.zip
-ln -s GoogleCastSDK-ios-4.4.7_dynamic GoogleCastSDK
-patch -p0 <strip.diff
+curl -OL https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-4.8.4_dynamic.zip
+unzip GoogleCastSDK-ios-4.8.4_dynamic.zip
+ln -s GoogleCastSDK-ios-4.8.4_dynamic_xcframework/GoogleCast.xcframework .
